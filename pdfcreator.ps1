c3d05508aaf3a631bf65728cd2c522de0f08f644
klvsmanvitha1234-hue <klvsmanvitha1234@gmail.com>
2026-05-29 23:02:05 +0530

    project

--- pdfcreator.ps1
+++
@@ -1,6 +0,0 @@
-#******************************2nd Project on exe file*******************************************
-$PDF_Installer="PDFCreator-6_2_2-Setup.exe"
-#*******************************Dynamic Process(user may can download in any foler)**************
-$Dyanamic_Path=Join-Path -Path $PSScriptRoot -ChildPath $PDF_Installer
-#*******************************silent installation process**************************************
-Start-Process -FilePath $Dyanamic_Path -ArgumentList "/COMPONENTS='program' /VERYSILENT /NORESTART" -NoNewWindow -Wait
