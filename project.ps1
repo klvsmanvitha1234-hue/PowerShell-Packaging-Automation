b646fe9dc7c6a7bf57a62c2df81e6ec1267738c0
klvsmanvitha1234-hue <klvsmanvitha1234@gmail.com>
2026-05-29 07:37:55 +0530

    Delete project.ps1

--- project.ps1
+++
@@ -1,8 +0,0 @@
-#*******************File Path******************************************
-$Node_Path="node-v24.16.0-x64.msi"
-$Dyanamic_Path=Join-Path -Path $PSScriptRoot -ChildPath $Node_Path
-#*******************Silent Command*************************
-$command="/i"+'"'+ $Dyanamic_Path+'"' +"/qn /norestart"
-#**********************Silent Installation process***********************
-Start-Process -FilePath "msiexec.exe" -ArgumentList $command -NoNewWindow -Wait
-#***********************Stop*****************************************************
